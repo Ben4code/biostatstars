2988be9a8f8168e905166f33e78bc6e4ffe4b849
ben4code <nnaemeka.obioha@gmail.com>
2021-10-03 02:34:51 +0100

    made changes

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
-import existingData from '../../data.json'
+import existingData from "../../data.json";
 
 export default (req, res) => {
- 
   if (req.method !== "POST") {
     res.status(405).send({ message: "Only POST requests allowed" });
     return;
   }
 
-  req.body.date = new Date().toLocaleDateString()
-  req.body.time = new Date().toLocaleTimeString()
-  fs.writeFileSync("./src/data.json", JSON.stringify([...existingData, req.body], null, 4));
-  res.status(200).json({ msg: "Thank you for contacting us, we will be in touch shortly." });
+  req.body.date = new Date().toLocaleDateString();
+  req.body.time = new Date().toLocaleTimeString();
+  fs.writeFileSync(
+    "./src/data.json",
+    JSON.stringify([...existingData, req.body], null, 4)
+  );
+  res.status(200).json({
+    msg: "Thank you for contacting us, we will be in touch shortly.",
+  });
 };
